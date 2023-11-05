614cd5208db034e37d95bf4af9dac421fa3485f6
Sidiq Hersi <hersisidiq@gmail.com>
2023-11-05 13:09:17 +0000

    adding the functionality to the start button

---
+++ assets/js/logic.js
@@ -0,0 +1,25 @@
+// Event Listeners
+document.getElementById('start').addEventListener('click', startQuiz);
+// quiz variables
+let currentQuestionIndex = 0;
+let timeLeft = 60; 
+// Initial time left for the quiz
+let score = 0; 
+let timerInterval;
+
+// html elements
+const startScreen = document.getElementById('start-screen');
+const questionsScreen = document.getElementById('questions');
+const endScreen = document.getElementById('end-screen');
+const finalScoreElement = document.getElementById('final-score');
+const feedbackElement = document.getElementById('feedback');
+
+// function to start the quiz
+function startQuiz(){
+    startScreen.classList.add('hide');
+    questionsScreen.classList.remove('hide');
+    startTimer();
+    displayQuestion();
+}
+
+
