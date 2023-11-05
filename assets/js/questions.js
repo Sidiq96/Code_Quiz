06a0e05a95577af39fb1cc78855297fc27146951
Sidiq Hersi <hersisidiq@gmail.com>
2023-11-05 12:25:29 +0000

    adding questions to the questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,34 @@
+// Quiz questions and answers
+const questions = [
+  {
+    question: "How many time zones are there in Russia? ",
+    answers: ["11", "15", "8", "10"],
+    correctAnswer: "11",
+  },
+  {
+    question: " Until 1923, what was the Turkish city of Istanbul called? ",
+    answers: ["Ankara", "Bursa,", "Constantinople", "Antalya"],
+    correctAnswer: "Constantinople",
+  },
+  {
+    question: "How many days does it take for the Earth to orbit the Sun? ",
+    answers: ["350", "355", "360", "365"],
+    correctAnswer: "365",
+  },
+  {
+    question: "Which country drinks the most amount of coffee per person? ",
+    answers: ["Italy", "Finland", "Colombia", "Sweden"],
+    correctAnswer: "Finland",
+  },
+  {
+    question:
+      "How many times per day does the average American open their fridge? ",
+    answers: ["5", "10", "12", "33"],
+    correctAnswer: "33",
+  },
+  {
+    question: "Who is credited with inventing the World Wide Web? ",
+    answers: ["Steve Jobs", "Bill Gates", "Tim Berners-Lee"],
+    correctAnswer: "Tim Berners-Lee",
+  },
+];
